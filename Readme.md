5ccc9efa2d01c6e23d908c616e83ac258673d9af
Michael R Wassermann <michael.wassermann@proton.me>
2025-07-04 14:19:27 -0500

    Updated

---
+++ Readme.md
@@ -0,0 +1,1 @@
+#Lead Developer: Michael Wassermann
--- obj/Debug/net8.0/gh-pages-app.AssemblyInfo.cs
+++ obj/Debug/net8.0/gh-pages-app.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("gh-pages-app")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6a0f1f8da6679e55c5c6cc6eded5c923b2741e0f")]
 [assembly: System.Reflection.AssemblyProductAttribute("gh-pages-app")]
 [assembly: System.Reflection.AssemblyTitleAttribute("gh-pages-app")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
